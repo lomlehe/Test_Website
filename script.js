6136c13d9430cb712d44f3bcb329c80aa8472c73
lomlehe <hbrhbkiri@gmail.com>
2026-05-27 11:20:37 +0800

    final working game

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-alert("Mini Game Pasal Boringgss");
-
 let correctAnswer = "";
 
-// Show Alphabet Game
+// Alphabet game
 function alphabetGame() {
     document.getElementById("gameArea").style.display = "block";
     document.querySelector(".games").style.display = "none";
@@ -11,7 +9,7 @@ function alphabetGame() {
     correctAnswer = "B";
 }
 
-// Show Number Game
+// Number game
 function numberGame() {
     document.getElementById("gameArea").style.display = "block";
     document.querySelector(".games").style.display = "none";
@@ -20,12 +18,20 @@ function numberGame() {
     correctAnswer = "2";
 }
 
-// Show Color Game
+// Color game
 function colorGame() {
     document.getElementById("gameArea").style.display = "block";
     document.querySelector(".games").style.display = "none";
 
     document.getElementById("question").innerText = "🌈 What color is the sky?";
+
+    // IMPORTANT: change answer buttons to words
+    document.querySelector(".answers").innerHTML = `
+        <button onclick="checkAnswer('red')">Red</button>
+        <button onclick="checkAnswer('blue')">Blue</button>
+        <button onclick="checkAnswer('green')">Green</button>
+    `;
+
     correctAnswer = "blue";
 }
 
@@ -40,10 +46,17 @@ function checkAnswer(answer) {
     }
 }
 
-// Go back to menu
+// Back to menu
 function goHome() {
     document.getElementById("gameArea").style.display = "none";
     document.querySelector(".games").style.display = "flex";
+
+    // reset answers back to A B C
+    document.querySelector(".answers").innerHTML = `
+        <button onclick="checkAnswer('A')">A</button>
+        <button onclick="checkAnswer('B')">B</button>
+        <button onclick="checkAnswer('C')">C</button>
+    `;
+
     document.getElementById("result").innerText = "";
 }
-
