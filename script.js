17e8a9d65270dc75ba54002b2646df71ba6931ba
lomlehe <hbrhbkiri@gmail.com>
2026-05-27 10:22:10 +0800

    first upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+function startGame() {
+    let answer = prompt("What comes after A?");
+
+    if (answer.toLowerCase() === "b") {
+        alert("Correct! 🎉");
+    } else {
+        alert("Oops! Try again 😊");
+    }
+}
+
+function alphabetGame() {
+    let answer = prompt("What comes after A?");
+    if (answer.toLowerCase() === "b") {
+        alert("Correct! 🎉");
+    } else {
+        alert("Try again!");
+    }
+}
+
+function numberGame() {
+    let answer = prompt("What comes after 1?");
+    if (answer === "2") {
+        alert("Correct! 🎉");
+    } else {
+        alert("Try again!");
+    }
+}
+
+function colorGame() {
+    let answer = prompt("What color is the sky?");
+    if (answer.toLowerCase() === "blue") {
+        alert("Correct! 🎉");
+    } else {
+        alert("Try again!");
+    }
+}
